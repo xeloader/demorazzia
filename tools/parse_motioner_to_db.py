e78184626a294b064c834ffb77fdac1e68498a2e
Victor Ingman <viregistermail@gmail.com>
2017-07-06 21:22:30 +0200

    Adding data to database and edge cases fixed

--- tools/parse_motioner_to_db.py
+++ tools/parse_motioner_to_db.py
@@ -7,15 +7,27 @@ import pprint
 url = "https://www.riksdagen.se"
 pp = pprint.PrettyPrinter(indent=4)
 
-# con = mysql.connect(
-#   host='127.0.0.1',
-#   database='demorazzia',
-#   port=8889,
-#   user='***REMOVED***',
-#   password='***REMOVED***')
+con = mysql.connect(
+  host='127.0.0.1',
+  database='demorazzia',
+  port=8889,
+  user='***REMOVED***',
+  password='***REMOVED***')
 
-for i in range(1, 512):
-  for j in range(0, 20):
+cur = con.cursor();
+
+PAGES = 512
+MOTIONS_PER_PAGE = 20
+START_AT = 189
+
+def propExists(motionId):
+  query = "SELECT id FROM propositions WHERE motion_id = %s";
+  cur.execute(query, [motionId])
+  motion = cur.fetchone()
+  return motion is not None
+
+for i in range(START_AT, PAGES + 1):
+  for j in range(0, MOTIONS_PER_PAGE):
     print("[parsing p%d-m%d]" % (i, j))
     motion = open('./motioner/p%s-m%s.html' % (i, j), 'r').read()
 
@@ -30,59 +42,113 @@ for i in range(1, 512):
     infos = info.findAll(class_="medium-smaller")
     
     handelser = []
+    motionCategory = None
+    dedicatedTo = None
 
     for idx in range(0, len(infos)):
-      handelser.append(infos[idx].get_text())
+      info = infos[idx].get_text()
+      if "motionskategori" in info.lower():
+        motionCategory = info
+      elif "tilldelat" in info.lower():
+        dedicatedTo = info
+      else:
+        handelser.append(info)
 
-    # motions_cat = infos[idx].get_text()
-    # tilldelat = info.findAll(class_="medium-smaller")[1].get_text()
-    # inlamnad = info.findAll(class_="medium-smaller")[2].get_text()
-    # bordlagd = info.findAll(class_="medium-smaller")[3].get_text()
-    # granskad = info.findAll(class_="medium-smaller")[4].get_text()
-    # hanvisad = info.findAll(class_="medium-smaller")[5].get_text()
+    motion_url = "%s%s" % (url, html.find(class_="aspnetForm")['action'])
     pdf_url = html.find(class_="link-file file-type-pdf")
+    if pdf_url is not None:
+      pdf_url = pdf_url['href']
     bodyHtml = html.find(class_="Section1")
     body = None
     if bodyHtml is not None:
-      body = bodyHtml.getText()
-    yrkanden = html.find(id="item0").getText()
+      body = bodyHtml.getText().strip()
+
+    yrkandenHtml = html.find(id="item0")
+    yrkanden = None
+    if yrkandenHtml is not None:
+      yrkandenHtml = yrkandenHtml.getText().strip()
 
 
     politicians = []
     phtmls = html.findAll(class_="fellow-item-container") # personer som skickade in motionen.
     for phtml in phtmls:
+      fullName = phtml.find(class_="fellow-name").text
+      poliName = re.sub(r'\([^)]*\)', '', fullName).strip()
       poli = {
         "picture_url": phtml.img['src'],
-        "url": phtml['href'],
-        "name": phtml.find(class_="fellow-name").text
+        "url": "%s%s" % (url, phtml['href']),
+        "name": poliName
       }
-      poli['party'] = re.findall('\((.*?)\)', poli['name'])[0] # extract party from name
+      poli['party'] = re.findall('\((.*?)\)', fullName)[0].upper() # extract party from name
       politicians.append(poli)
 
+    cur.execute("INSERT INTO propositions "
+                "(type, body, title, motion_id, url, pdf_url, dedicated_to, yrkanden, sender) "
+                "VALUES "
+                "(%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                (motionCategory, body, title, motion_id, motion_url, pdf_url, dedicatedTo, yrkanden, senders))
+    propId = cur.lastrowid
+    print("Added prop with id %d" % propId)
 
-    pp.pprint(politicians)
-    pp.pprint(handelser)
+    for politician in politicians:
+      personName = politician['name']
+      personParty = politician['party']
+      partyId = None
+      personId = None
+
+      # Check if party exists.
+      cur.execute("SELECT id FROM parties WHERE symbol = %s", [personParty])
+      partyData = cur.fetchone();
+      if partyData is not None: # add partyId to variable
+        partyId = partyData[0]
+      else: # add party
+        query = ("INSERT INTO parties "
+                "(symbol) "
+                "VALUES "
+                "(%s)")
+        cur.execute(query, [personParty])
+        partyId = cur.lastrowid
+        print("Added party %s to db with id %d" % (personParty, partyId))
+
+      # Check if politician exists.
+      query = "SELECT id, name FROM politicians WHERE name = %s"
+      cur.execute(query, [personName])
+      p = cur.fetchone()
+      if p is not None: # add to personId variable
+         personId = p[0]
+      else: # add politician to db
+        # print("V has the id %s" % party_id[0])
+        query = ("INSERT INTO politicians"
+                "(party_id, name, picture_url, url)"
+                "VALUES"
+                "(%s, %s, %s, %s)")
+        cur.execute(query, [partyId, personName, politician['picture_url'], politician['url']])
+        personId = cur.lastrowid
+        print("Added person %s to db with id %d" % (personName, personId))
+
+      query = "INSERT INTO proposition_senders (proposition_id, politician_id) VALUES (%s, %s)";
+      cur.execute(query, [propId, personId])
+      print("Added politician %s as sender to propId %d as relationId %d" % (personName, propId, cur.lastrowid))
+
+    print("Added %d senders to prop %s (%d)" % (len(politicians), title, propId))
+
+    con.commit()
+
+    # pp.pprint(politicians)
+    # pp.pprint(handelser)
+    # print(motionCategory)
+    # print(dedicatedTo)
     # print(motions_cat, tilldelat, inlamnad, bordlagd, granskad, hanvisad, sep='\n', end='\n')
-    print(pdf_url, end='\n')
-    print(title, motion_id, senders, sep='\n')
+    # print(pdf_url, end='\n')
+    # print(motion_url, end='\n')
+    # print(title, motion_id, senders, sep='\n')
     print("------------------")
 
 
+
+cur.close()
+con.close()
 exit();
-cur = con.cursor();
-
-query = ("SELECT * FROM proposition_types")
-cur.execute(query)
-
-add_proposition = ("INSERT INTO propositions "
-                  "(first_name, last_name, hire_date, gender, birth_date) "
-                  "VALUES (%s, %s, %s, %s, %s)")
-
-for (id, constant, name, description, parent) in cur:
-  print("{}, {}, {}, {}, {}".format(id, constant, name, description, parent))
-
-cur.close();
-con.close();
 
 
 # for i in range(1,sidor):
